2238be97a7b2a36e9b201a29c85754a0c108ba46
Kipuru <purumkim339@gmail.com>
2026-06-10 14:40:23 -0700

    Implement basic start & stop timer functions

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
 let isRunning = false;
+let remainingSecs = 1500;
 
-document.getElementById("start-button").addEventListener("click", handleButtonPress);
+let timerVar;
 
-function handleButtonPress() {
-    console.log("button pressed!");
+const startButton = document.getElementById("start-button");
+startButton.addEventListener("click", handleButtonPress);
+
+function handleButtonPress(event) {
+    isRunning = !isRunning;
+    if (isRunning) {
+        timerVar = window.setInterval(perSecond, 1000);
+        console.log("timer started");
+        startButton.innerText = "Stop";
+    }
+    else {
+        clearInterval(timerVar);
+        console.log("timer stopped/ended");
+        startButton.innerText = "Start";
+    }
+}
+
+function perSecond() {
+    remainingSecs--;
+    console.log(remainingSecs);
 }
